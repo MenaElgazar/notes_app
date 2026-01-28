310a9360edf784bfefedacc46fa2eb3dbde9f9fd
MenaElgazar <menaelgazar72@gmail.com>
2026-01-28 18:01:32 +0200

    create type_adapter

--- lib/models/node_model.dart
+++ lib/models/node_model.dart
@@ -1,8 +1,26 @@
+import 'package:hive/hive.dart';
+
+part 'node_model.g.dart';
+
+@HiveType(typeId: 0)
 class NodeModel {
+
+  @HiveField(0)
   final String title;
+
+  @HiveField(1)
   final String subtitle;
+
+  @HiveField(2)
   final String date;
+
+  @HiveField(3)
   final int color;
 
-  NodeModel({required this.title, required this.subtitle, required this.date, required this.color});
+  NodeModel({
+    required this.title,
+    required this.subtitle,
+    required this.date,
+    required this.color,
+  });
 }
